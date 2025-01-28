a7a6785e2d700c1a1f1751443c24508edb33c271
Lara Rodrigues <rodrigueslarab@gmail.com>
2025-01-27 23:52:58 -0300

    Add files via upload

---
+++ Elysium Imóveis V1.9/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'login_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado ao MySQL!');
+});
+
+module.exports = connection;
